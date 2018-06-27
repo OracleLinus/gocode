226e225398812f8fb0da126446c66eb04ea01b01
Linus <linushakansson89@gmail.com>
2018-06-27 12:22:56 +0200

    Update service.go

--- service.go
+++ service.go
@@ -1,32 +1,11 @@
 package main
 import ("github.com/ant0ine/go-json-rest/rest"
 	"fmt"
-	"database/sql"
-	_ "github.com/go-sql-driver/mysql"
 	"io/ioutil"
 	"net/http"
 	"os")
 func main() {
-    // try making database connection and query
-    db, err := sql.Open("mysql", "user:pass@tcp(ip:port)/databasename")
-    defer db.Close()
-
-    if err != nil {
-        fmt.Println("Failed to connect", err)
-        return
-    }
-	
-	var xCoordinate int
-	var yCoordinate int
-	err = db.QueryRow("SELECT * from Sample").Scan(&xCoordinate, &yCoordinate)
-	switch {
-	case err == sql.ErrNoRows:
-	case err != nil:
-		fmt.Println("Failed to get results" ,err)
-	default:
-        fmt.Println(xCoordinate, yCoordinate)
-	}
-	
+ 
 	// try making http request against sample site
 	response, err := http.Get("http://ip.jsontest.com/")
 	if err != nil {
